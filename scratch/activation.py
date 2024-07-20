3fd38698c7bf469eff5c6dd539f303549833541b
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-07-20 22:17:53 +0200

    added cnn from scratch

---
+++ scratch/activation.py
@@ -0,0 +1,63 @@
+from layer import Layer
+import numpy as np
+
+class Activation(Layer):
+    def __init__(self, activation, activation_prime):
+        self.activation = activation
+        self.activation_prime = activation_prime
+    
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
+    
+    def backward(self, output_gradient, learning_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
+
+### SOME ACTIVATION FUNCTIONS ###
+
+class Tanh(Activation):
+    def __init__(self):
+        def tanh(x):
+            return np.tanh(x)
+
+        def tanh_prime(x):
+            return 1 - np.tanh(x) ** 2
+
+        super().__init__(tanh, tanh_prime)
+        
+class Sigmoid(Activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+
+        super().__init__(sigmoid, sigmoid_prime)
+
+class Softmax(Layer):
+    def forward(self, input):
+        tmp = np.exp(input)
+        self.output = tmp / np.sum(tmp)
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        # This version is faster than the one presented in the video
+        n = np.size(self.output)
+        return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
+
+
+### SOME LOSS FUNCTIONS ###
+
+def mse(y_true, y_pred):
+    return np.mean(np.power(y_true - y_pred, 2))
+
+def mse_prime(y_true, y_pred):
+    return 2 * (y_pred - y_true) / np.size(y_true)
+
+def binary_cross_entropy(y_true, y_pred):
+    return np.mean(-y_true * np.log(y_pred) - (1 - y_true) * np.log(1 - y_pred))
+
+def binary_cross_entropy_prime(y_true, y_pred):
+    return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
