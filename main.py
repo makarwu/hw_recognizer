6bb8891cbe8ce291cc47b3f8ca76b0793da96420
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-07-26 22:00:09 +0200

    added frontend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import torch
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from PIL import Image
 from models import HCRM
 from torchvision import transforms
@@ -20,7 +20,7 @@ model.eval()
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+"""@app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
         return jsonify({'error': 'No file provided'}), 400
@@ -31,7 +31,27 @@ def predict():
     img = transform(img).unsqueeze(0).to(device)
     output = model(img)
     _, predicted = torch.max(output, 1)
-    return jsonify({'digits': predicted.item()}) #MNIST numbers
+    return jsonify({'digits': predicted.item()}) #MNIST numbers"""
+
+@app.route('/')
+def home():
+    return render_template('/Users/makarwuckert/Desktop/hw_recognizer/index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return "No file part"
+
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    
+    if file:
+        img = Image.open(io.BytesIO(file.read()))
+        img = transform(img).unsqueeze(0).to(device)
+        output = model(img)
+        _, predicted = torch.max(output, 1)
+        return render_template('/Users/makarwuckert/Desktop/hw_recognizer/result.html', text=predicted.item())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
