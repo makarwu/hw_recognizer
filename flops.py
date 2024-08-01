6d68ba9bd230a7d4935d6ee85b3466ec3463bc12
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-01 21:57:37 +0200

    added flop analysis

---
+++ flops.py
@@ -0,0 +1,22 @@
+import torch
+from thop import profile
+from models import HCRM
+from importlib_metadata import version
+
+### FLOPS ANALYSIS ###
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = HCRM().to(device)
+model.load_state_dict(torch.load('./model/handwritten_character_recognition_model.pth'))
+
+print("thop version:", version("thop"))
+
+# MACS = multi-accumulate operations (typically counted as 2 flops)
+# ->> one multiply and one accumulate
+input_tensor = torch.randn(1, 1, 28, 28).to(device)
+macs, params = profile(model, inputs=(input_tensor,), verbose=False)
+flops = 2*macs
+print(f"{flops:.1e} FLOPS")
+
+del model
+torch.cuda.empty_cache()
